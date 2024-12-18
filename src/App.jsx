37b9067ad70a6dcc7feed874142ec8dd3c4e779f
AliElsese <ali.elsese104@gmail.com>
2024-12-18 06:31:01 -0800

    update v

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Statistics from './pages/Statistics';
+import Groups from './pages/Groups';
+import NavBar from './components/NavBar';
+import SideBar from './components/SideBar';
+import StudentsInfo from './pages/StudentsInfo';
+import Employees from './pages/Employees';
+import Classes from './pages/Classes';
+import Attendences from './pages/Attendences';
+import Grades from './pages/Grades';
+import Finance from './pages/Finance';
+import GroupDetails from './pages/GroupDetails';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
@@ -9,9 +19,11 @@ function App() {
   return (
     <div className='App'>
       <BrowserRouter>
-        <Routes>
-          <Route path='/' >
-            <Route index element={<Home />} />
+      <NavBar />
+        <div className="d-flex">
+          <SideBar />
+            <Routes>
+              <Route path='/' >
                 <Route index element={<Statistics />} />
                 <Route path='statistics' element={<Statistics />} />
                 <Route path='classes' element={<Classes />} />
@@ -22,11 +34,13 @@ function App() {
                 <Route path='attendences' element={<Attendences />} />
                 <Route path='grades' element={<Grades />} />
                 <Route path='finance' element={<Finance />} />
-            <Route path='login' element={<Login />} />
-            <Route path='register' element={<Register />} />
-          </Route>
-          <Route path='*' element={<h1 className='vh-100 d-flex align-items-center justify-content-center'>Eror 404 | Not Found</h1>} />
-        </Routes>
+                <Route path='login' element={<Login />} />
+                <Route path='register' element={<Register />} />
+                <Route path='*' element={<h1 className='vh-100 d-flex align-items-center justify-content-center'>Eror 404 | Not Found</h1>} />
+              </Route>
+            </Routes>
+        </div>
+        
       </BrowserRouter>
     </div>
   )
